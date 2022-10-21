c5f8d52feef2c713deb86ad9c9118a62b907a95d
SebastianG <carchiog.sebastian@gmail.com>
2022-10-21 20:03:15 -0300

    agregado de font

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log(" ¡Que Chusma!");
+
+
+let preg = prompt(" ¿Sos alumno de CoderHouse? \n\n 1. Si \n 2. No");
+
+if (preg == "Si") {
+    alert(" Tenes descuentos para accesorios y/o service");
+} else {
+    alert(" Podes continuar con tu navegacion. Muchas Gracias");
+}
+
