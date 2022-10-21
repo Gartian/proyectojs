07546f57cea465f8db651e64005d070134a2fa8b
SebastianG <carchiog.sebastian@gmail.com>
2022-10-21 20:16:44 -0300

    Correccion de if - Js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ console.log(" ¡Que Chusma!");
 let preg = prompt(" ¿Sos alumno de CoderHouse? \n\n 1. Si \n 2. No");
 
 if (preg == 1) {
-    alert(" Tenes descuentos para accesorios y/o service");
+    alert(" Tenes descuentos en accesorios y/o service");
 } else {
     alert(" Podes continuar con tu navegacion. Muchas Gracias");
 }
